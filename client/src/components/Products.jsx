cd8bf3f33ac4c31e34a80fb664164aa023bbb291
rhmndhika <rahmndhika@student.umn.ac.id>
2023-06-13 11:29:55 +0700

    update sorting function and useEffect in Products

--- client/src/components/Products.jsx
+++ client/src/components/Products.jsx
@@ -37,7 +37,7 @@ const Products = ({cat,filter,sort}) => {
           )
         )
       );
-  }, [products, cat, filter])
+  }, [products, cat, filter, sort])
 
   useEffect(() => {
     const insertionSort = (arr) => {
@@ -59,10 +59,10 @@ const Products = ({cat,filter,sort}) => {
             swap(arr, j, j - 1);
             j--;
           }
-        } else {
+        } else if (sort === "Rating") {
           while (
             j > 0 &&
-            getAverageRating(arr[j - 1].reviews) < getAverageRating(arr[j].reviews)
+            getSumRating(arr[j - 1].reviews) < getSumRating(arr[j].reviews)
           ) {
             swap(arr, j, j - 1);
             j--;
@@ -78,26 +78,32 @@ const Products = ({cat,filter,sort}) => {
       arr[j] = temp;
     };
   
-    const getAverageRating = (reviews) => {
+    const getSumRating = (reviews) => {
       if (!reviews || reviews.length === 0) return 0;
   
       let sum = 0;
       for (let i = 0; i < reviews.length; i++) {
         sum += reviews[i].rating;
       }
-      return sum / reviews.length;
+      return sum;
     };
   
     setFilteredProducts((prev) => {
       const sortedProducts = insertionSort([...prev]);
-      const filteredProducts = sortedProducts.filter(
-        (product) => product.reviews.length > 0
-      );
+      let filteredProducts;
+      if (sort === "Rating") {
+        filteredProducts = sortedProducts.filter(
+          (product) => product.reviews.length > 0
+        );
+      } else {
+        filteredProducts = sortedProducts;
+      }
       return filteredProducts;
     });
   }, [sort]);
   
 
+
   // useEffect(() => {
   //   const insertionSort = (arr) => {
   //     const n = arr.length;
@@ -105,41 +111,83 @@ const Products = ({cat,filter,sort}) => {
   //       let j = i;
   //       if (sort === "Newest") {
   //         while (j > 0 && arr[j - 1].createdAt < arr[j].createdAt) {
-  //           // Swap arr[j] and arr[j-1]
-  //           const temp = arr[j];
-  //           arr[j] = arr[j - 1];
-  //           arr[j - 1] = temp;
+  //           swap(arr, j, j - 1);
   //           j--;
   //         }
   //       } else if (sort === "Asc") {
   //         while (j > 0 && arr[j - 1].price > arr[j].price) {
-  //           // Swap arr[j] and arr[j-1]
-  //           const temp = arr[j];
-  //           arr[j] = arr[j - 1];
-  //           arr[j - 1] = temp;
+  //           swap(arr, j, j - 1);
   //           j--;
   //         }
   //       } else if (sort === "Desc") {
   //         while (j > 0 && arr[j - 1].price < arr[j].price) {
-  //           // Swap arr[j] and arr[j-1]
-  //           const temp = arr[j];
-  //           arr[j] = arr[j - 1];
-  //           arr[j - 1] = temp;
+  //           swap(arr, j, j - 1);
+  //           j--;
+  //         }
+  //       } else if (sort == "Rating") {
+  //         while (
+  //           j > 0 &&
+  //           getAverageRating(arr[j - 1].reviews) < getAverageRating(arr[j].reviews)
+  //         ) {
+  //           swap(arr, j, j - 1);
+  //           j--;
+  //         }
+  //       }
+  //     }
+  //     return arr;
+  //   };
+  
+  //   const swap = (arr, i, j) => {
+  //     const temp = arr[i];
+  //     arr[i] = arr[j];
+  //     arr[j] = temp;
+  //   };
+  
+  //   const getAverageRating = (reviews) => {
+  //     if (!reviews || reviews.length === 0) return 0;
+  
+  //     let sum = 0;
+  //     for (let i = 0; i < reviews.length; i++) {
+  //       sum += reviews[i].rating;
+  //     }
+  //     return sum / reviews.length;
+  //   };
+  
+  //   setFilteredProducts((prev) => {
+  //     const sortedProducts = insertionSort([...prev]);
+  //     const filteredProducts = sortedProducts.filter(
+  //       (product) => product.reviews.length > 0
+  //     );
+  //     return filteredProducts;
+  //   });
+  // }, [sort]);
+  
+  // useEffect(() => {
+  //   const insertionSort = (arr) => {
+  //     const n = arr.length;
+  //     for (let i = 1; i < n; i++) {
+  //       let j = i;
+  //       if (sort === "Newest") {
+  //         while (j > 0 && arr[j - 1].createdAt < arr[j].createdAt) {
+  //           swap(arr, j, j - 1);
+  //           j--;
+  //         }
+  //       } else if (sort === "Asc") {
+  //         while (j > 0 && arr[j - 1].price > arr[j].price) {
+  //           swap(arr, j, j - 1);
+  //           j--;
+  //         }
+  //       } else if (sort === "Desc") {
+  //         while (j > 0 && arr[j - 1].price < arr[j].price) {
+  //           swap(arr, j, j - 1);
   //           j--;
   //         }
   //       } else {
   //         while (
   //           j > 0 &&
-  //           (
-  //             !arr[j - 1].reviews.length ||
-  //             !arr[j - 1].reviews[0] ||
-  //             arr[j - 1].reviews[0].rating < arr[j].reviews[0]?.rating
-  //           )
+  //           getAverageRating(arr[j - 1].reviews) < getAverageRating(arr[j].reviews)
   //         ) {
-  //           // Swap arr[j] and arr[j-1]
-  //           const temp = arr[j];
-  //           arr[j] = arr[j - 1];
-  //           arr[j - 1] = temp;
+  //           swap(arr, j, j - 1);
   //           j--;
   //         }
   //       }
@@ -147,143 +195,36 @@ const Products = ({cat,filter,sort}) => {
   //     return arr;
   //   };
   
-  //   setFilteredProducts((prev) => insertionSort([...prev]));
-  // }, [sort]);
-
-  // useEffect(() => {
-  //   cat &&
-  //     setFilteredProducts(
-  //       products.filter((item) => {
-  //         const { price, reviews, materials } = item;
-  //         return (
-  //           (!filter.price || price.includes(filter.price)) &&
-  //           (!filter.materials || materials.includes(filter.materials)) &&
-  //           (sort !== "Rating" || (reviews && reviews.length > 0))
-  //         );
-  //       })
-  //     );
-  // }, [products, cat, filter, sort]);
-
- 
- 
-  // useEffect(() => {
-  //   const insertionSort = (arr) => {
-  //     const n = arr.length;
-  //     for (let i = 1; i < n; i++) {
-  //       let j = i;
-  //       if (sort === "Newest") {
-  //         while (j > 0 && arr[j - 1].createdAt < arr[j].createdAt) {
-  //           // Swap arr[j] and arr[j-1]
-  //           const temp = arr[j];
-  //           arr[j] = arr[j - 1];
-  //           arr[j - 1] = temp;
-  //           j--;
-  //         }
-  //       } else if (sort === "Asc") {
-  //         while (j > 0 && arr[j - 1].price > arr[j].price) {
-  //           // Swap arr[j] and arr[j-1]
-  //           const temp = arr[j];
-  //           arr[j] = arr[j - 1];
-  //           arr[j - 1] = temp;
-  //           j--;
-  //         }
-  //       } else if (sort === "Desc") {
-  //         while (j > 0 && arr[j - 1].price < arr[j].price) {
-  //           // Swap arr[j] and arr[j-1]
-  //           const temp = arr[j];
-  //           arr[j] = arr[j - 1];
-  //           arr[j - 1] = temp;
-  //           j--;
-  //         }
-  //       } else if (sort === "Rating") {
-  //         while (
-  //           j > 0 &&
-  //           (
-  //             !arr[j - 1].reviews.length ||
-  //             !arr[j - 1].reviews[0] ||
-  //             getAverageRating(arr[j - 1].reviews) < getAverageRating(arr[j].reviews)
-  //           )
-  //         ) {
-  //           // Swap arr[j] and arr[j-1]
-  //           const temp = arr[j];
-  //           arr[j] = arr[j - 1];
-  //           arr[j - 1] = temp;
-  //           j--;
-  //         }
-  //       }
-  //     }
-  //     return arr;
-  //   };
-
-  //   const getAverageRating = (reviews) => {
-  //     const sum = reviews.reduce((total, review) => total + review.rating, 0);
-  //     const average = sum / reviews.length;
-  //     return average;
+  //   const swap = (arr, i, j) => {
+  //     const temp = arr[i];
+  //     arr[i] = arr[j];
+  //     arr[j] = temp;
   //   };
   
-  //   setFilteredProducts((prev) => insertionSort([...prev]));
-  // }, [sort]);
-
-  // useEffect(() => {
-  //   const insertionSort = (arr) => {
-  //     const n = arr.length;
-  //     for (let i = 1; i < n; i++) {
-  //       let j = i;
-  //       if (sort === "Newest") {
-  //         while (j > 0 && arr[j - 1].createdAt < arr[j].createdAt) {
-  //           // Swap arr[j] and arr[j-1]
-  //           const temp = arr[j];
-  //           arr[j] = arr[j - 1];
-  //           arr[j - 1] = temp;
-  //           j--;
-  //         }
-  //       } else if (sort === "Asc") {
-  //         while (j > 0 && arr[j - 1].price > arr[j].price) {
-  //           // Swap arr[j] and arr[j-1]
-  //           const temp = arr[j];
-  //           arr[j] = arr[j - 1];
-  //           arr[j - 1] = temp;
-  //           j--;
-  //         }
-  //       } else if (sort === "Desc") {
-  //         while (j > 0 && arr[j - 1].price < arr[j].price) {
-  //           // Swap arr[j] and arr[j-1]
-  //           const temp = arr[j];
-  //           arr[j] = arr[j - 1];
-  //           arr[j - 1] = temp;
-  //           j--;
-  //         }
-  //       } else if (sort === "Rating") {
-  //         while (j > 0 && (!hasReviews(arr[j - 1]) || !hasReviews(arr[j]) || getAverageRating(arr[j - 1].reviews) < getAverageRating(arr[j].reviews))) {
-  //           // Swap arr[j] and arr[j-1]
-  //           const temp = arr[j];
-  //           arr[j] = arr[j - 1];
-  //           arr[j - 1] = temp;
-  //           j--;
-  //         }
-  //       }
-  //     }
-  //     return arr;
-  //   };
-
-  //   const hasReviews = (product) => {
-  //     return product.reviews && product.reviews.length > 0;
-  //   };
-
   //   const getAverageRating = (reviews) => {
-  //     const sum = reviews.reduce((total, review) => total + review.rating, 0);
-  //     const average = sum / reviews.length;
-  //     return average;
+  //     if (!reviews || reviews.length === 0) return 0;
+  
+  //     let sum = 0;
+  //     for (let i = 0; i < reviews.length; i++) {
+  //       sum += reviews[i].rating;
+  //     }
+  //     console.log(sum / reviews.length, reviews)
+  //     return sum / reviews.length;
   //   };
-    
-
-  //   if (sort === "Rating") {
-  //     const filteredAndSortedProducts = insertionSort(filteredProducts.filter(hasReviews));
-  //     setFilteredProducts(filteredAndSortedProducts);
-  //   } else {
-  //     setFilteredProducts((prev) => insertionSort([...prev]));
-  //   }
-  // }, [sort, filteredProducts]);
+  
+  //   setFilteredProducts((prev) => {
+  //     const sortedProducts = insertionSort([...prev]);
+  //     let filteredProducts;
+  //     if (sort === "Rating") {
+  //       filteredProducts = sortedProducts.filter(
+  //         (product) => product.reviews.length > 0
+  //       );
+  //     } else {
+  //       filteredProducts = sortedProducts;
+  //     }
+  //     return filteredProducts;
+  //   });
+  // }, [sort]);
   
 
   return (
